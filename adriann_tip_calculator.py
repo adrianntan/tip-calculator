88d9a78d0ba902cb35b3e0c5fb7ce12c689e0412
adrianntan <135498508+adrianntan@users.noreply.github.com>
2024-07-24 12:46:06 -0500

    Update adriann_tip_calculator.py

--- adriann_tip_calculator.py
+++ adriann_tip_calculator.py
@@ -3,6 +3,6 @@ total_bill = input("What was the total bill?\n$")
 percent_tip = input("What percentage tip would you like to give? 10, 12, or 15?\n")
 number_of_people = input("How many people to split the bill?\n")
 
-tip = round((float(total_bill)/int(number_of_people))*(1 + (float(percent_tip)/100)), 2)
+bill_with_tip = round((float(total_bill)/int(number_of_people))*(1 + (float(percent_tip)/100)), 2)
 
-print(f"Each person has to pay: ${tip}")
+print(f"Each person has to pay: ${bill_with_tip}")
